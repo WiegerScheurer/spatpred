ae5a21ea38ea55165a28865aada525912e5bfa0c
WiegerScheurer <wieger.scheurer1@gmail.com>
2024-02-04 21:28:38 +0100

    added func to acquire stimulus design matrix per subj

--- funcs/imgproc.py
+++ funcs/imgproc.py
@@ -2,6 +2,10 @@ import os
 import matplotlib.pyplot as plt
 import random
 import h5py
+import numpy as np
+# from funcs.imgproc import show_stim
+from scipy.io import loadmat
+
 
 # Function to show a randomly selected image of the nsd dataset
 def show_stim(hide = 'n', img_no = 'random'):
@@ -30,3 +34,35 @@ def show_stim(hide = 'n', img_no = 'random'):
         plt.show()
         
     return test_image, image_no
+
+# Create design matrix containing ordered indices of stimulus presentation per subject
+def get_imgs_designmx():
+    
+    subjects = os.listdir('/home/rfpred/data/natural-scenes-dataset/nsddata/ppdata')
+    exp_design = '/home/rfpred/data/natural-scenes-dataset/nsddata/experiments/nsd/nsd_expdesign.mat'
+    
+    # Load MATLAB file
+    mat_data = loadmat(exp_design)
+
+    # Order of the presented 30000 stimuli, first 1000 are shared between subjects, rest is randomized (1, 30000)
+    # The values take on values betweeon 0 and 1000
+    img_order = mat_data['masterordering']-1
+
+    # The sequence of indices from the img_order list in which the images were presented to each subject (8, 10000)
+    # The first 1000 are identical, the other 9000 are randomly selected from the 73k image set. 
+    img_index_seq = (mat_data['subjectim'] - 1) # Change from matlab to python's 0-indexing
+    
+    # Create design matrix for the subject-specific stimulus presentation order
+    stims_design_mx = {}
+    stim_list = np.zeros((img_order.shape[1]))
+    for n_sub, subject in enumerate(sorted(subjects)):
+    
+        for stim in range(0, img_order.shape[1]):
+            
+            idx = img_order[0,stim]
+            stim_list[stim] = img_index_seq[n_sub, idx]
+            
+        stims_design_mx[subject] = stim_list.astype(int)
+    
+    return stims_design_mx
+    
