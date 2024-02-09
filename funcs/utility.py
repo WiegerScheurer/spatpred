254ea39e9b53307fb698ae2e2808b154d0df7f00
WiegerScheurer <wieger.scheurer1@gmail.com>
2024-02-09 14:15:02 +0100

    RMS z-score transformation

--- funcs/utility.py
+++ funcs/utility.py
@@ -10,3 +10,17 @@ def print_dict_structure(d, indent=0):
 def print_large(item):
     with np.printoptions(threshold=np.inf):
         print(item)
+        
+        
+def get_zscore(data, print_ars = 'y'):
+    mean_value = np.mean(data)
+    std_dev = np.std(data)
+
+    # Calculate z-scores
+    z_scores = (data - mean_value) / std_dev
+
+    if print_ars == 'y':
+        print("Original array:", data)
+        print("Z-scores:", z_scores)
+        
+    return z_scores
