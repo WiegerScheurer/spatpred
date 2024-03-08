57b9e16a1ffc9cb2d4d059570948fb32d0a19274
WiegerScheurer <wieger.scheurer1@gmail.com>
2024-03-08 10:07:35 +0100

    intermediate save

--- funcs/imgproc.py
+++ funcs/imgproc.py
@@ -355,3 +355,66 @@ def get_img_prf(image, x = None, y = None, sigma = None, type = 'gaussian', heat
     
     return prf_info #, weibull_pars
 
+# Code to acquire the hrf parameters for each subject, roi, voxel
+# Importantly, it allows working with the data without crashing (though only for max 3 sessions at a time). 
+# It loads in the nifti files, extracts the required data, overwrites it.
+def get_betas(subjects, voxels, start_session, end_session):
+    beta_dict = {}
+    
+    if subjects == 'all':
+        subjects = [f'subj{i:02d}' for i in range(1, 9)]
+    else:
+        subjects = [subjects]
+    for subject in subjects:
+        beta_dict[subject] = {}
+        
+        rois = list(voxels[subject].keys())
+
+        hrf_betas = {}
+        
+        for session in range(start_session, end_session):
+            session += 1
+            if session < 10:
+                session = f'0{session}'
+            
+            # session_nifti = betas_ses1 # Uncomment to check functionality of the code, if betas_ses1 has been loaded before.
+            session_nifti = (nib.load(f'/home/rfpred/data/natural-scenes-dataset/nsddata_betas/ppdata/{subject}/func1mm/betas_fithrf_GLMdenoise_RR/betas_session{session}.nii.gz')).get_fdata(caching = 'unchanged')
+            n_imgs = session_nifti.shape[3]
+        
+            print(f'Working on session: {session} of subject: {subject}')
+            for roi in rois: 
+                
+                if session == '01':
+                    hrf_betas[roi] = {}
+                    # beta_dict[subject][roi] = {}
+        
+                voxel_mask = voxels[subject][roi] # These is the boolean mask for the specific subject, roi
+                n_voxels = np.sum(voxel_mask).astype('int') # This is the amount of voxels in this roi
+                vox_indices = np.zeros([n_voxels, voxel_mask.ndim], dtype = int) # Initiate an empty array to store vox indices                
+                
+                for coordinate in range(vox_indices.shape[1]): # Fill the array with the voxel coordinates as indices
+                    vox_indices[:, coordinate] = np.where(voxel_mask == 1)[coordinate]
+                    
+                for voxel in range(n_voxels):
+                    vox_idx = vox_indices[voxel] # Get the voxel indices for the current voxel
+                
+                    hrf_betas_ses = (np.array(session_nifti[tuple(vox_idx)]).reshape(n_imgs, 1))/300
+                    
+                    if session == '01':
+                        hrf_betas[roi][f'voxel{voxel + 1}'] = hrf_betas_ses
+                        
+                    total_betas = np.append(hrf_betas[roi][f'voxel{voxel + 1}'], hrf_betas_ses)
+                    
+                    hrf_betas[roi][f'voxel{voxel + 1}'] = total_betas
+                
+            with open('./data/custom_files/intermediate_hrf_save.pkl', 'wb') as fp:
+                pickle.dump(hrf_betas, fp)
+                print('     - Back-up saved to intermediate_hrf_save.pkl\n')
+                    
+        beta_dict[subject] = hrf_betas               
+        
+    with open('./data/custom_files/beta_dict.pkl', 'wb') as fp:
+        pickle.dump(beta_dict, fp)
+        print('     - Back-up saved to beta_dict.pkl\n')        
+                
+    return beta_dict
