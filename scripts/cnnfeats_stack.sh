2c4d8f420a7ccdc34e9303ed6e221509ddcdaad5
WiegerScheurer <wieger.scheurer1@gmail.com>
2024-04-28 20:27:35 +0200

    fixed get_ccnfeats.py script

--- scripts/cnnfeats_stack.sh
+++ scripts/cnnfeats_stack.sh
@@ -16,11 +16,19 @@ source activate /home/rfpred/envs/rfenv
 
 
 # Iterate over the first argument from 0 to 29000 in steps of 1000
-for arg1 in $(seq 0 1200 28800); do
+for arg1 in $(seq 0 500 29500); do
     # Calculate arg2 as arg1 + 250
-    arg2=$((arg1 + 1200))
+    arg2=$((arg1 + 500))
 
     # Run the Python script with the current arguments
-    /home/rfpred/scripts/get_cnnfeats.py $arg1 $arg2 'subj01' 7
+    /home/rfpred/scripts/get_cnnfeats.py $arg1 $arg2 'subj01' 9
 done
 
+# # Iterate over the first argument from 0 to 29000 in steps of 1000
+# for arg1 in $(seq 0 500 1000); do
+#     # Calculate arg2 as arg1 + 250
+#     arg2=$((arg1 + 500))
+
+#     # Run the Python script with the current arguments
+#     /home/rfpred/scripts/get_cnnfeats.py $arg1 $arg2 'subj01' 9
+# done
--- scripts/get_cnnfeats.py
+++ scripts/get_cnnfeats.py
@@ -26,9 +26,9 @@ from torchvision.models.feature_extraction import create_feature_extractor, get_
 from tqdm import tqdm
 import traceback
 
-torch.manual_seed(1)
-random.seed(1)
-np.random.seed(1)
+# torch.manual_seed(1)
+# random.seed(1)
+# np.random.seed(1)
 
 
 os.chdir('/home/rfpred')
@@ -39,6 +39,7 @@ sys.path.append('/home/rfpred/envs/rfenv/lib/python3.11/site-packages/nsdcode')
 print(sys.path)
 
 from funcs.imgproc import rand_img_list, show_stim, get_imgs_designmx
+from funcs.utility import get_zscore
 
 
 predparser = argparse.ArgumentParser(description='Get the AlexNet feature space neural representations for a range of images of a subject')
@@ -66,7 +67,7 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, idx):
         img_id = self.image_ids[idx]
-        imgnp = (show_stim(img_no=img_id, hide='y', small = 'y')[0]) #[163:263,163:263])
+        imgnp = (show_stim(img_no=img_id, hide='y', small = 'y')[0])[163:263,163:263]
         
         imgPIL = Image.fromarray(imgnp) # Convert into PIL from np
 
@@ -91,14 +92,38 @@ feature_extractor = create_feature_extractor(model, return_nodes=[this_layer])
 
 start = args.start
 end = args.end
-batch_size = 600
+batch_size = end - start
+n_comps = 500
 image_ids = get_imgs_designmx()[args.subject][start:end]
 dataset = ImageDataset(image_ids, transform=preprocess)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
+# def fit_pca(feature_extractor, dataloader):
+#     # Define PCA parameters
+#     pca = IncrementalPCA(n_components=n_comps, batch_size=batch_size)
+
+#     while True:  # Keep trying until successful
+#         try:
+#             # Fit PCA to batch
+#             for _, d in tqdm(enumerate(dataloader), total=len(dataloader)):
+#                 # Extract features
+#                 ft = feature_extractor(d)
+#                 # Flatten the features
+#                 ft = torch.hstack([torch.flatten(l, start_dim=1) for l in ft.values()])
+#                 if np.isnan(ft.detach().numpy().any()):
+#                     raise ValueError("Nan value detected before PCA fit")
+#                 # Print out some summary statistics of the features
+#                 print(f'Mean: {ft.mean()}, Std: {ft.std()}, Min: {ft.min()}, Max: {ft.max()}')
+#                 # Fit PCA to batch
+#                 pca.partial_fit(ft.detach().cpu().numpy())
+#             return pca  # Return the PCA object
+#         except Exception as e:
+#             print(f"Error occurred: {e}")
+#             print("Restarting PCA fitting...")
+
 def fit_pca(feature_extractor, dataloader):
     # Define PCA parameters
-    pca = IncrementalPCA(n_components=600, batch_size=batch_size)
+    pca = IncrementalPCA(n_components=n_comps, batch_size=batch_size)
 
     while True:  # Keep trying until successful
         try:
@@ -108,6 +133,18 @@ def fit_pca(feature_extractor, dataloader):
                 ft = feature_extractor(d)
                 # Flatten the features
                 ft = torch.hstack([torch.flatten(l, start_dim=1) for l in ft.values()])
+                
+                # Check for NaN values
+                if np.isnan(ft.detach().numpy().any()):
+                    raise ValueError("NaN value detected before PCA fit")
+                
+                # Check for extreme outliers
+                if (ft.detach().numpy() < -100000).any() or (ft.detach().numpy() > 100000).any():
+                    raise ValueError("Extreme outlier detected before PCA fit")
+                
+                # Print out some summary statistics of the features
+                print(f'Mean: {ft.mean()}, Std: {ft.std()}, Min: {ft.min()}, Max: {ft.max()}')
+                
                 # Fit PCA to batch
                 pca.partial_fit(ft.detach().cpu().numpy())
             return pca  # Return the PCA object
@@ -129,9 +166,16 @@ def extract_features(feature_extractor, dataloader, pca):
                 # Flatten the features
                 ft = torch.hstack([torch.flatten(l, start_dim=1) for l in ft.values()])
 
+                # Print out some summary statistics of the features
+                print(f'Mean: {ft.mean()}, Std: {ft.std()}, Min: {ft.min()}, Max: {ft.max()}')
+
                 # Check if the features contain NaN values
                 if np.isnan(ft.detach().numpy()).any():
                     raise ValueError("NaN value detected")
+                
+                # Check for extreme outliers
+                if (ft.detach().numpy() < -100000).any() or (ft.detach().numpy() > 100000).any():
+                    raise ValueError("Extreme outlier detected before PCA fit")
                 
                 # Apply PCA transform
                 ft = pca.transform(ft.cpu().detach().numpy())
@@ -151,6 +195,7 @@ del model, pca
 
 print('Deleted model and pca object to save memory')
 
+# Here I stack all the stuff
 if args.end == 30000:
     prf_region = 'center_strict'
     
@@ -162,25 +207,28 @@ if args.end == 30000:
             total_arrays += len(data.files)
 
     # Create layer7_feats with the correct size
-    layer_feats = np.zeros((total_arrays, 600))
+    layer_feats = np.zeros((total_arrays, n_comps))
 
     n_file = -1
     current_array = 0
     for file_name in sorted(os.listdir(f'/home/rfpred/data/custom_files/{args.subject}/{prf_region}/')):
         n_file += 1
-        if file_name.startswith(f"cnn_pcs_layerfeatures{args.cnn_layer}4") and file_name.endswith(".npz"):
+        if file_name.startswith(f"cnn_pcs_layerfeatures.{args.cnn_layer}") and file_name.endswith(".npz"):
             data = np.load(f'/home/rfpred/data/custom_files/{args.subject}/{prf_region}/{file_name}')
             n_imgs = len(data.files)
             
             if n_imgs > 0:
-                session_ar = np.zeros((n_imgs, 600))
+                session_ar = np.zeros((n_imgs, n_comps))
                 for image in range(n_imgs):
-                    session_ar[image, :] = data[f'arr_{image}']
+                    session_ar[image, :] = get_zscore(data[f'arr_{image}'], print_ars='n')
 
                 layer_feats[current_array:current_array+n_imgs, :] = session_ar
                 current_array += n_imgs
                 
                 
+                
+    # Print out some summary statistics of the features
+    print(f'All feat stats:\nMean: {layer_feats.mean()}, Std: {layer_feats.std()}, Min: {layer_feats.min()}, Max: {layer_feats.max()}')
     # if layer_feats.shape == (30000, 600):
     np.save(f'/home/rfpred/data/custom_files/subj01/center_strict/alex_lay{args.cnn_layer}.npy', layer_feats)
     print(f'Saved yet another collection of extracted features, this time from layer {args.cnn_layer}')
