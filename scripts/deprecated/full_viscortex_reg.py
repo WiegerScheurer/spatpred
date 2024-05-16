48a7ae9ab8698c1fbc31b8907fd74f071bf83f79
WiegerScheurer <wieger.scheurer1@gmail.com>
2024-05-16 22:59:02 +0200

    black linting, encoding bash runs

---
+++ scripts/deprecated/full_viscortex_reg.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+# This is an old script, likely doesn't work anymore and is also not needed as it leverages outdated regression approaches
+
+import os
+import sys
+os.environ["OMP_NUM_THREADS"] = "10"
+
+os.chdir('/home/rfpred')
+sys.path.append('/home/rfpred/')
+sys.path.append('/home/rfpred/envs/rfenv/lib/python3.11/site-packages/')
+sys.path.append('/home/rfpred/envs/rfenv/lib/python3.11/site-packages/nsdcode')
+
+import pickle
+
+import importlib
+from importlib import reload
+import funcs.natspatpred
+importlib.reload(funcs.natspatpred)
+from funcs.natspatpred import NatSpatPred
+
+NSP = NatSpatPred()
+NSP.initialise()
+
+rois, roi_masks = NSP.cortex.visrois_dict()
+prf_dict = NSP.cortex.prf_dict(rois, roi_masks)
+
+y_dict_all, xyz_to_vox_all = NSP.analyse.get_hrf_dict('subj01', voxels=roi_masks, prf_region = 'full_viscortex', 
+                                             min_size = 0.1, max_size = 10, prf_proc_dict=prf_dict, max_voxels = None ,plot_sizes = 'n',
+                                             vismask_dict=roi_masks, minimumR2=0, in_perc_signal_change=False)
+
+y_all, xyzs_all = NSP.analyse.load_y(subject='subj01', 
+                             roi=None, 
+                             hrf_dict=y_dict_all, 
+                             xyz_to_name=xyz_to_vox_all, 
+                             roi_masks=roi_masks, 
+                             prf_dict=prf_dict, 
+                             n_voxels='all', 
+                             start_img=0, 
+                             n_imgs=3750, # Only extracted betas across visual cortex for the first 5 sessions 
+                             verbose=True,
+                             across_rois=True)
+n_imgs = 3750
+X = NSP.stimuli.unet_featmaps([2, 3])
+alf = .01
+cv = 5
+
+results = NSP.analyse.evaluate_model(X[:n_imgs,:], y[:n_imgs], alpha=alf, cv=cv)
+# results['predicted_values']
+
+_ = NSP.analyse.plot_learning_curve(X[:n_imgs,:], y[:n_imgs], model=results['model'], alpha=alf, cv=cv)
+# NSP.analyse.plot_feature_importance(X, y, model=results['model'], alpha=.1)
+# NSP.analyse.plot_residuals(X, y, model=results['model'], alpha=alf)
+results
+
+
+# Save the results
+with open('results_23.pkl', 'wb') as f:
+    pickle.dump(results, f)
+
+# Save the xyz_to_vox_all
+with open('xyz_to_vox_all.pkl', 'wb') as f:
+    pickle.dump(xyz_to_vox_all, f)
+    
+print(f'Het is weer gelukt')
+
+X = NSP.stimuli.unet_featmaps([3])
+alf = .01
+cv = 5
+
+results = NSP.analyse.evaluate_model(X[:n_imgs,:], y[:n_imgs], alpha=alf, cv=cv)
+# results['predicted_values']
+
+_ = NSP.analyse.plot_learning_curve(X[:n_imgs,:], y[:n_imgs], model=results['model'], alpha=alf, cv=cv)
+# NSP.analyse.plot_feature_importance(X, y, model=results['model'], alpha=.1)
+# NSP.analyse.plot_residuals(X, y, model=results['model'], alpha=alf)
+results
+
+
+# Save the results
+with open('results_3.pkl', 'wb') as f:
+    pickle.dump(results, f)
+
+X = NSP.stimuli.unet_featmaps([2])
+alf = .01
+cv = 5
+
+results = NSP.analyse.evaluate_model(X[:n_imgs,:], y[:n_imgs], alpha=alf, cv=cv)
+# results['predicted_values']
+
+_ = NSP.analyse.plot_learning_curve(X[:n_imgs,:], y[:n_imgs], model=results['model'], alpha=alf, cv=cv)
+# NSP.analyse.plot_feature_importance(X, y, model=results['model'], alpha=.1)
+# NSP.analyse.plot_residuals(X, y, model=results['model'], alpha=alf)
+results
+
+
+# Save the results
+with open('results_2.pkl', 'wb') as f:
+    pickle.dump(results, f)
---
+++ scripts/store_enc_feats.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+# This script pulls the feature maps from the specified layer of the CNN for each subject and makes them
+# regression-ready. It saves the feature maps in the 'encoding' folder of the subject-specific location.
+# All matrices are Z-scored per feature. This script should only be run once so that the feature maps are
+# more easily accessible as numpy.ndarrays that can be loaded in in minimal time.
+
+import os
+
+# Limit the number of CPUs used to 5
+os.environ["OMP_NUM_THREADS"] = "5"
+
+import sys
+import numpy as np
+from scipy.stats import zscore as zs
+
+os.chdir("/home/rfpred")
+sys.path.append("/home/rfpred")
+sys.path.append("/home/rfpred/envs/rfenv/lib/python3.11/site-packages/")
+sys.path.append("/home/rfpred/envs/rfenv/lib/python3.11/site-packages/nsdcode")
+
+from funcs.natspatpred import NatSpatPred
+
+NSP = NatSpatPred()
+NSP.initialise()
+
+
+def _pull_featmaps(subject: str, cnn_layer: str, verbose: bool = False):
+    """Function to pull the feature maps from the specified layer of the CNN, for the specified number of voxels.
+        The raw files are dicts that result from the encoding_stack.sh script, with ['arr_{image_number}'] as keys.
+        This function transforms these dicts into a 2d numpy that can be used as X matrix for the regression analysis.
+        It is saved at the subject-specific location in the 'encoding' folder. Should only be used once per subject.
+
+    Args:
+    - subject (str): The subject for which the feature maps are pulled.
+    - cnn_layer (str): The layer of the CNN from which the feature maps are pulled.
+    - verbose (bool): Whether to print out the progress of the function.
+
+    Out:
+    - Xsubj (np.array): The feature maps for the specified subject and layer.
+    """
+    # Load in the feature maps for the specified layer of the CNN, dimensionality reduction has already been applied
+    featmaps_pc = np.load(
+        f"{NSP.own_datapath}/visfeats/cnn_featmaps/featmaps/featmaps_layfeatures.{cnn_layer}.npz"
+    )
+
+    # Convert the dictionary-like object to a numpy array
+    Xpca = np.array([zs(featmaps_pc[key]) for key in featmaps_pc])
+    if verbose:
+        NSP.utils.inspect_dat(Xpca)
+
+    # Subject specific indices, 30k images
+    subj_ices = [NSP.stimuli.imgs_designmx()[subject]]
+    subj_dmx = Xpca[subj_ices[0]]
+    Xsubj = zs(subj_dmx, axis=0)  # Z-score the feature maps for every image
+
+    os.makedirs(f"{NSP.own_datapath}/{subject}/encoding", exist_ok=True)
+    np.save(
+        "%s/%s/encoding/regprepped_featmaps_layer%s"
+        % (NSP.own_datapath, subject, cnn_layer),
+        Xsubj,
+    )
+    if verbose:
+        print(f"Saved the feature maps for subject {subject} and layer {cnn_layer}.")
+
+    return Xsubj
+
+
+# The layers of AlexNet that are ReLU layers, others weren't extracted (yet)
+relu_lays = [1, 4, 7, 9, 11]
+
+for subject in NSP.subjects:
+    for cnn_layer in relu_lays:
+        _ = _pull_featmaps(subject, cnn_layer, verbose=True)
+
+print("Ook dit script heeft de klus wederom geklaard, chapeau!")
