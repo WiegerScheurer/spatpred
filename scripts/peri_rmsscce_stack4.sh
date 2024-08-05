90aa7a289528c25e59c0ae4170f0311c69fc23e7
WiegerScheurer <wieger.scheurer1@gmail.com>
2024-08-05 15:34:53 +0200

    peripheral patch feature computation for 6 patches

--- scripts/peri_rmsscce_stack4.sh
+++ scripts/peri_rmsscce_stack4.sh
@@ -4,7 +4,8 @@ eccentricity=2
 angle=90
 # [90.0, 150.0, 210.0, 270.0, 330.0, 30.0]
 
-for startimg in $(seq 0 1000 72000); do
+# for startimg in $(seq 0 1000 72000); do
+for startimg in $(seq 21000 1000 72000); do
     endimg=$((startimg + 1000))
 
     echo "Computing the local contrast features for the peripheral patch at $angle degrees, with $eccentricity eccentricity, startimg $startimg, endimg $endimg"
