bd04d8cb355e611db7329288303c3019b26b07eb
WiegerScheurer <wieger.scheurer1@gmail.com>
2024-09-11 19:33:02 +0200

    intermed save

--- scripts/pred_ridge_peri_stack.sh
+++ scripts/pred_ridge_peri_stack.sh
@@ -11,6 +11,7 @@ for angle in 330; do
 
     for subj_num in {01..08}; do
         echo "Running peripheral unpredictability ridge regression for subj$subj_num"
-        /home/rfpred/scripts/run_pred_ridge_peri.py subj$subj_num $eccentricity $angle --mean_unpred
+        # /home/rfpred/scripts/run_pred_ridge_peri.py subj$subj_num $eccentricity $angle --mean_unpred
+        /home/rfpred/scripts/run_pred_ridge_peri.py subj$subj_num $eccentricity $angle
     done
 done
